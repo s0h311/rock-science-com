4b6ea68b2d4adc83ff172e60ae225628df6ba35d
Soheil Nazari <kontakt@soheilnazari.de>
2026-03-02 00:04:56 +0100

    update homepage

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
-    <header>
+  <div class="flex flex-col items-center p-3 gap-10">
+    <header class="mx-auto">
       <NuxtImg
         src="/rs-logo.webp"
-        width="80"
-        height="80"
+        width="70"
+        height="70"
       />
     </header>
 
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,25 @@
-<template></template>
+<template>
+  <div class="grid place-items-center gap-10">
+    <h1 class="text-4xl italic">
+      <span class="font-serif">We build</span>
+      what others overcomplicate.
+    </h1>
+
+    <NuxtLink
+      class="w-fit space-y-3 rounded-2xl shadow p-3 hover:scale-105 transition duration-300 hover:underline text-start"
+      to="https://tabley.de"
+      target="_blank"
+    >
+      <NuxtImg
+        class="rounded-2xl"
+        src="/tabley-logo.webp"
+        width="200"
+        height="200"
+      />
+
+      <span>tabley.de</span>
+    </NuxtLink>
+  </div>
+</template>
 
 <script setup lang="ts"></script>
