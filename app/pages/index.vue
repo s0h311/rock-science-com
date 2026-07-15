3b4572fa3c8255c619333259f3432c2530d0f1f0
Soheil Nazari <kontakt@soheilnazari.de>
2026-07-15 11:32:13 +0200

    update projects

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,23 +1,35 @@
 <template>
-  <div class="grid place-items-center gap-10">
-    <h1 class="text-4xl italic text-center">
-      <span class="font-serif">We build</span>
-      what others overcomplicate.
-    </h1>
+  <h1 class="text-4xl italic text-center mb-10">
+    <span class="font-serif">We build</span>
+    what others overcomplicate.
+  </h1>
 
+  <div class="grid md:grid-cols-2 lg:grid-cols-3 place-items-center gap-10">
     <NuxtLink
+      v-for="project in projects"
+      :key="project.name"
       class="w-fit space-y-3 rounded-2xl shadow p-3 hover:scale-105 transition duration-300 hover:underline text-start"
-      to="https://tabley.de"
+      :to="project.url"
       target="_blank"
     >
-      <NuxtImg
-        class="rounded-2xl"
-        src="/tabley-logo.webp"
-        width="200"
-        height="200"
-      />
+      <div
+        v-if="project.image"
+        class="size-50"
+      >
+        <NuxtImg
+          class="rounded-2xl size-full object-contain"
+          :src="project.image"
+        />
+      </div>
 
-      <span>tabley.de</span>
+      <div
+        v-else
+        class="size-50"
+      >
+        {{ project.name }}
+      </div>
+
+      <span>{{ project.name }}</span>
     </NuxtLink>
   </div>
 </template>
@@ -35,4 +47,26 @@ useSeoMeta({
   twitterDescription: 'We build what others overcomplicate. Building tech you love in Hamburg.',
   twitterImage: '/rs-logo-sky-1024x256.webp',
 })
+
+const projects: {
+  name: string
+  url: string
+  image: string | null
+}[] = [
+  {
+    name: 'tabley.de',
+    url: 'https://tabley.de',
+    image: '/tabley-logo.webp',
+  },
+  {
+    name: 'eco-on.de',
+    url: 'https://eco-on.de',
+    image: '/eco-on-logo-1.png',
+  },
+  {
+    name: 'StatusLine',
+    url: 'https://statusline.rock-science.com',
+    image: null,
+  },
+]
 </script>
